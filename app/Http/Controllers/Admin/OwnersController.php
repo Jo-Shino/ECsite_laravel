c7279a80e3ea73ffbe581901288d51b34277b119
Jo-Shino <joe000042@gmail.com>
2022-04-17 17:06:06 +0900

    index, create

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -23,13 +23,13 @@ class OwnersController extends Controller
 
     public function index()
     {
-        $data_now = Carbon::now()->year;
-        $data_parse = Carbon::parse(now());
-        echo $data_now;
-        echo $data_parse;
+        // $data_now = Carbon::now()->year;
+        // $data_parse = Carbon::parse(now());
+        // echo $data_now;
+        // echo $data_parse;
 
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name','created_at')->get();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name','created_at')->get();
         // $q_first = DB::table('owners')->select('name')->first();
 
         // $c_test = collect([
@@ -37,8 +37,9 @@ class OwnersController extends Controller
         // ]);
 
         // dd($e_all, $q_get, $q_first, $c_test);
+        $owners = Owner::select('name','email','created_at')->get();
         return view('admin.owners.index', 
-        compact('e_all','q_get'));
+        compact('owners'));
     }
 
     /**
@@ -49,7 +50,7 @@ class OwnersController extends Controller
     
      public function create()
     {
-        //
+        return view('admin.owners.create');
     }
 
     /**
